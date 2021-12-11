532c53d26975750894b9bcdb042d19e554ed20de
Navi-n-Don <navi-n-don@ukr.net>
2021-12-11 21:50:27 +0200

    views

--- someapp/urls.py
+++ someapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-
+from . import views
 
 urlpatterns = [
-
+    path('company_list/', views.CompanyListView.as_view(), name='fulllist'),
+    path('company_list/<str:slug>/', views.CompanyDetailView.as_view(), name='company-details'),
 ]
--- someapp/views.py
+++ someapp/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from .models import Company, Phone, Email
+from django.views.generic import ListView, DetailView, UpdateView, CreateView, DeleteView, FormView
 
-# Create your views here.
+
+class CompanyListView(ListView):
+    model = Company
+    template_name = "someapp/fulllist.html"
+    context_object_name = "companies"
+    paginate_by = 2
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['phones'] = Phone.objects.all()
+        context['emails'] = Email.objects.all()
+        return context
+
+
+class CompanyDetailView(DetailView):
+    model = Company
+    template_name = "someapp/company-details.html"
+    context_object_name = "company"
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['phones'] = Phone.objects.all()
+        context['emails'] = Email.objects.all()
+        return context
